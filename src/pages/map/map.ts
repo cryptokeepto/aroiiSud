da1a9154ab8ac0ff702dd59b475f8f1a748d613f
cryptokeepto <miikeoho@gmail.com>
2019-01-27 15:45:44 +0700

    success

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -16,11 +16,7 @@ export class MapPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
-  }
-
-  ionViewDidEnter() {
+  ionViewWillEnter() {
     this.getCurrentLocation()
 
   }
@@ -34,19 +30,56 @@ export class MapPage {
       pitch: 80,
       minZoom: 7.5, //restrict map zoom - buildings not visible beyond 13
       maxZoom: 17,
-      container: 'map'
+      container: 'map',
+
     });
+    map.on('load', function () {
+      map.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+          'fill-extrusion-color': '#aaa',
+          'fill-extrusion-height': {
+            'type': 'identity',
+            'property': 'height'
+          },
+          'fill-extrusion-base': {
+            'type': 'identity',
+            'property': 'min_height'
+          },
+          'fill-extrusion-opacity': .6
+        }
+      });
+    });
+
+
+    var html = '<ion-card>I am here</ion-card>';
+
+    var customPopUp = new mapboxgl.Popup(
+      {
+        anchor: 'bottom',   // To show popup on top
+        offset: { 'bottom': [0, -10] },  // To prevent popup from over shadowing the marker.
+        closeOnClick: false   // To prevent close on mapClick.
+      }
+    ).setHTML(html);
+    new mapboxgl.Marker()
+      .setLngLat([this.Coordinates.longitude, this.Coordinates.latitude])
+      .setPopup(customPopUp)
+      .addTo(map);
   }
 
   private getCurrentLocation() {
     let options = {
-      frequency: 3000,
+      frequency: 1000,
       enableHighAccuracy: true
     };
 
     this.watch = Geolocation.watchPosition(options)
       .subscribe((position: Geoposition) => {
-        console.log(position);
         this.Coordinates = position.coords;
         this.executeMap()
       });
